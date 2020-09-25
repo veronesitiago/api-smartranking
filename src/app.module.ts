9c19f9a57aeeb8ac043870a8de7f237c53874ed0
tiago <tiagoveronesi@outcenter.com.br>
2020-09-25 17:12:49 -0300

    Ocultar url de DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:5LxgkDP5c0IjotVE@cluster0.bclie.mongodb.net/smartRanking?retryWrites=true&w=majority',
+      'url',
       {
         useNewUrlParser: true,
         useCreateIndex: true,
