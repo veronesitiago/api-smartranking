fb57c7542c7af90fcd7f69ac24a765cc4c4d00b2
tiago <tinacio@meudroz.com>
2022-05-30 23:27:27 -0300

    chore: manipulação de erros [filter] e alteração de diretório do [pipe]

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse();
+    const request = context.getRequest();
+
+    const statusCode =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    this.logger.error(
+      `Http status: ${statusCode} Error Message: ${JSON.stringify(message)}`,
+    );
+
+    response.status(statusCode).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
